1bd06fdf47774e637d1f0639d0fbffb1d9444023
C-Rodg <rodgersbc@gmail.com>
2017-11-29 10:01:02 -0800

    Created basic express route

---
+++ README.md
@@ -0,0 +1,8 @@
+# IIS Preview Generator
+
+A landing page for creating IIS virtual directories and applications using
+NodeJS.
+
+## Contributors
+
+Curtis Rodgers (me) : https://curtisrodgers.com
---
+++ index.js
@@ -0,0 +1,23 @@
+// Imports
+const express = require("express"),
+	fs = require("fs"),
+	path = require("path"),
+	bodyParser = require("body-parser");
+const app = express();
+
+// Configure express middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.static(__dirname + "/public"));
+
+// Send application page
+app.get("/validar/create-preview", (req, res) => {
+	res.sendFile(__dirname + "/public/index.html");
+});
+
+// Launch server
+app.listen(3002, () => {
+	console.log(
+		"Validar Preview Generator application is listening on port 3002"
+	);
+});
